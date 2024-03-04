d92391a1962802f787e52315241d1a10e9dac8db
hiisuuii <dogglez7777@gmail.com>
2024-03-03 22:18:48 -0700

    upd upd upd

--- babynames/cmake-build-debug/CMakeFiles/3.27.8/CMakeCCompiler.cmake
+++ babynames/cmake-build-debug/CMakeFiles/3.27.8/CMakeCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_C_COMPILER "C:/mingw-w64/mingw64/bin/gcc.exe")
+set(CMAKE_C_COMPILER "H:/Program Files (x86)/CLion 2023.3.3/bin/mingw/bin/gcc.exe")
 set(CMAKE_C_COMPILER_ARG1 "")
 set(CMAKE_C_COMPILER_ID "GNU")
 set(CMAKE_C_COMPILER_VERSION "13.1.0")
@@ -21,11 +21,11 @@ set(CMAKE_C_SIMULATE_VERSION "")
 
 
 
-set(CMAKE_AR "C:/mingw-w64/mingw64/bin/ar.exe")
-set(CMAKE_C_COMPILER_AR "C:/mingw-w64/mingw64/bin/gcc-ar.exe")
-set(CMAKE_RANLIB "C:/mingw-w64/mingw64/bin/ranlib.exe")
-set(CMAKE_C_COMPILER_RANLIB "C:/mingw-w64/mingw64/bin/gcc-ranlib.exe")
-set(CMAKE_LINKER "C:/mingw-w64/mingw64/bin/ld.exe")
+set(CMAKE_AR "H:/Program Files (x86)/CLion 2023.3.3/bin/mingw/bin/ar.exe")
+set(CMAKE_C_COMPILER_AR "H:/Program Files (x86)/CLion 2023.3.3/bin/mingw/bin/gcc-ar.exe")
+set(CMAKE_RANLIB "H:/Program Files (x86)/CLion 2023.3.3/bin/mingw/bin/ranlib.exe")
+set(CMAKE_C_COMPILER_RANLIB "H:/Program Files (x86)/CLion 2023.3.3/bin/mingw/bin/gcc-ranlib.exe")
+set(CMAKE_LINKER "H:/Program Files (x86)/CLion 2023.3.3/bin/mingw/bin/ld.exe")
 set(CMAKE_MT "")
 set(CMAKE_TAPI "CMAKE_TAPI-NOTFOUND")
 set(CMAKE_COMPILER_IS_GNUCC 1)
@@ -68,7 +68,7 @@ endif()
 
 
 
-set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "C:/mingw-w64/mingw64/lib/gcc/x86_64-w64-mingw32/13.1.0/include;C:/mingw-w64/mingw64/lib/gcc/x86_64-w64-mingw32/13.1.0/include-fixed;C:/mingw-w64/mingw64/x86_64-w64-mingw32/include")
-set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "mingw32;gcc;moldname;mingwex;kernel32;advapi32;shell32;user32;kernel32;iconv;mingw32;gcc;moldname;mingwex;kernel32")
-set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "C:/mingw-w64/mingw64/lib/gcc/x86_64-w64-mingw32/13.1.0;C:/mingw-w64/mingw64/lib/gcc;C:/mingw-w64/mingw64/x86_64-w64-mingw32/lib;C:/mingw-w64/mingw64/lib")
+set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "H:/Program Files (x86)/CLion 2023.3.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include;H:/Program Files (x86)/CLion 2023.3.3/bin/mingw/include;H:/Program Files (x86)/CLion 2023.3.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include-fixed;H:/Program Files (x86)/CLion 2023.3.3/bin/mingw/x86_64-w64-mingw32/include")
+set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "")
+set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "")
 set(CMAKE_C_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
--- babynames/main.cpp
+++ babynames/main.cpp
@@ -3,25 +3,63 @@
 #include <string>
 #include <fstream>
 #include <cassert>
-#include <cstring>
 #include <list>
+#include <iomanip>
+#include <algorithm>
 using namespace std;
 
-int findKey(map<int, int>& Map, int key) {
-
-    // Traverse the map
-    for (auto& it : Map) {
-
-        // If mapped value is K,
-        // then print the key value
+string findKey(const map<int, string>& t_map, const string &key) {
+    for (auto& it : t_map) {
         if (it.second == key) {
-            return it.first;
+            return to_string(it.first);
         }
     }
+    return "N/A";
+}
 
-    // If there is not key mapped with K,
-    // then print -1
-    return -1;
+void print_debug_one(const string &name, map<int, string> &map1, map<int, string> &map2, map<int, string> &map3, map<int, string> &map4){
+    string brank2012 = findKey(map1, name);
+    string grank2012 = findKey(map2, name);
+    string brank2022 = findKey(map3, name);
+    string grank2022 = findKey(map4, name);
+
+    cout << setw(16) << left << name << setw(16) << left << brank2012
+            << setw(16) << left << grank2012
+            << setw(16) << left << brank2022
+            << setw(16) << left << grank2022
+            << endl;
+
+}
+
+[[maybe_unused]] void print_debug_all(list<string> &list, map<int, string> &map1, map<int,string> &map2, map<int, string> &map3, map<int,string> &map4){
+    cout << setw(16) << left << "Name"
+         << setw(16) << left << "Boys 2012 rank"
+         << setw(16) << left << "Girls 2012 rank"
+         << setw(16) << left << "Boys 2022 rank"
+         << setw(16) << left << "Girls 2022 rank" << endl;
+    cout << string(16*4, '-');
+    for(const auto& name : list){
+        print_debug_one(name, map1, map2, map3, map4);
+    }
+}
+
+void pretty_print_name(const list<string>& names_list, const string& name, const map<int, string>* ranks_maps){
+    if(std::find(names_list.begin(), names_list.end(), name) != names_list.end()){
+        string rank = findKey(ranks_maps[0], name);
+
+        string res = findKey(ranks_maps[0], name);
+        string rank1 = res != "N/A" ? ("ranked " + res + " in popularity among boys in 2012") : "not ranked among the top 1000 names for boys in 2012.";
+        res = findKey(ranks_maps[1], name);
+        string rank2 = res != "N/A" ? ("ranked " + res + " in popularity among girls in 2012") : "does not rank among the top 1000 names for girls in 2012.";
+
+        res = findKey(ranks_maps[2], name);
+        string rank3 = res != "N/A" ? (res + " in popularity among boys in 2022") : "not ranked among the top 1000 names for boys in 2022.";
+        res = findKey(ranks_maps[3], name);
+        string rank4 = res != "N/A" ? (res + " in popularity among girls in 2022") : "does not rank among the top 1000 names for girls in 2022.";
+
+        cout << name << " is " << rank1 << " and " << rank3 << endl;
+        cout << name << " is " << rank2 << " and " << rank4 << endl;
+    }
 }
 
 int main() {
@@ -30,47 +68,42 @@ int main() {
     map<int, string> boys2022;
     map<int, string> girls2022;
 
+
+    int number;
+    string name1, name2;
+
     ifstream instream;
 
-    instream.open("C:\\Users\\doggl\\Desktop\\CSC2034-HAllen\\babynames\\babynames2012.txt");
+    /* Read 2012 names */
+    instream.open(".\\babynames2012.txt"); // same dir as .exe
     if (!instream.is_open()) {
         std::cerr << "Error opening file." << std::endl;
         assert(false);
     }
 
-    int number;
-    string name1, name2;
-
+    /* Store 2012 names */
     while (instream >> number >> name1 >> name2) {
         boys2012[number] = name1;
         girls2012[number] = name2;
     }
     instream.close();
-
-    instream.open("C:\\Users\\doggl\\Desktop\\CSC2034-HAllen\\babynames\\babynames2022.txt");
+    /* Read 2022 names */
+    instream.open(".\\babynames2022.txt"); // same dir as .exe
     if (!instream.is_open()) {
         std::cerr << "Error opening file." << std::endl;
         assert(false);
     }
-
+    /* Store 2022 names */
     while (instream >> number >> name1 >> name2) {
         boys2022[number] = name1;
         girls2022[number] = name2;
     }
     instream.close();
 
-    // Displaying the contents of map1
-    std::cout << "\n2012:\n";
-    for (const auto& entry : boys2012) {
-        std::cout << entry.first << ": " << entry.second << " / " << girls2012[entry.first] << endl;
-    }
-    std::cout << "\n2022:\n";
-    for (const auto& entry : boys2022) {
-        std::cout << entry.first << ": " << entry.second << " / " << girls2022[entry.first] << endl;
-    }
-
     list<string> ls;
-    for(const auto& entry : boys2012){
+
+    /* add all names to 1 list */
+    for(const auto& entry :boys2012){
         ls.push_back(entry.second);
     }
     for(const auto& entry : girls2012){
@@ -82,16 +115,32 @@ int main() {
     for(const auto& entry : girls2022){
         ls.push_back(entry.second);
     }
-    ls.sort();
-    ls.unique();
-    for(const auto& name : ls){
-        cout << name << " ";
-    }
-    cout << ls.size();
-    for(const auto& name : ls){
-        cout << name <<
-    }
+    ls.sort(); // required to deduplicate
+    ls.unique(); // deduplicate
 
+    /* i don't want to have to pass 4 maps to functions */
+    map<int, string> maps[] = { boys2012, girls2012, boys2022, girls2022};
+
+    string inname;
+    cout << "Enter name (capitalize first letter):" << endl;
+    cin >> inname;
+    pretty_print_name(ls, inname, maps);
+
+    /* write all ranked names to CSV file */
+    ofstream outfile;
+    outfile.open(".\\compareNames.txt", ios::out);
+    if(outfile.is_open()) {
+        outfile << "NAME,BOYS 2012 RANK,GIRLS 2012 RANK,BOYS 2022 RANK,GIRLS 2022 RANK" << endl;
+        for (const auto &name: ls) {
+            outfile << name
+                    << "," << findKey(boys2012, name)
+                    << "," << findKey(girls2012, name)
+                    << "," << findKey(boys2022, name)
+                    << "," << findKey(girls2022, name)
+                    << endl;
+        }
+    }
+    outfile.close();
 
     return 0;
 }
